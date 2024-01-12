4c9f6fd9e47e342181cbc7aa0fe5a4cf16c176e1
MattiaVolonterio <volonteriomattia@gmail.com>
2024-01-12 16:44:29 +0100

    feat: dice icon and reset

--- Dadi/js/main.js
+++ Dadi/js/main.js
@@ -1,14 +1,22 @@
 const userDice = document.getElementById("user-dice");
 const computerDice = document.getElementById("computer-dice");
 const randomizeButton = document.getElementById("randomize-button");
+const resetButton = document.getElementById("reset-button");
 const winner = document.getElementById("winner");
 
+let userDiceValue;
+let computerDiceValue;
+
 randomizeButton.addEventListener("click", function () {
-  const userDiceValue = Math.floor(Math.random() * 6 + 1);
-  const computerDiceValue = Math.floor(Math.random() * 6 + 1);
+  const randomUserDiceValue = Math.floor(Math.random() * 6 + 1);
+  const randomComputerDiceValue = Math.floor(Math.random() * 6 + 1);
+
+  userDiceValue = `bi-dice-${randomUserDiceValue}`;
+  computerDiceValue = `bi-dice-${randomComputerDiceValue}-fill`;
+
   let winnerValue = "";
-  userDice.innerText = userDiceValue;
-  computerDice.innerText = computerDiceValue;
+  userDice.classList.replace(`bi-dice-1`, userDiceValue);
+  computerDice.classList.replace(`bi-dice-1-fill`, computerDiceValue);
 
   if (userDiceValue > computerDiceValue) {
     winnerValue = "Utente";
@@ -19,4 +27,16 @@ randomizeButton.addEventListener("click", function () {
   }
 
   winner.innerText = `Il Vincitore è: ${winnerValue}`;
+
+  randomizeButton.classList.replace("d-inline-block", "d-none");
+  resetButton.classList.replace("d-none", "d-inline-block");
+});
+
+resetButton.addEventListener("click", function () {
+  userDice.classList.replace(userDiceValue, `bi-dice-1`);
+  computerDice.classList.replace(computerDiceValue, `bi-dice-1-fill`);
+  randomizeButton.classList.replace("d-none", "d-inline-block");
+  resetButton.classList.replace("d-inline-block", "d-none");
+
+  winner.innerText = "Il Vincitore è:";
 });
